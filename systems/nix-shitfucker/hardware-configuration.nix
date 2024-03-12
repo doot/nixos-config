fb49fb39e485b3313d30cab6596ce138f93f0eb9
Jeremy Hauschildt <jeremy@jhauschildt.com>
2024-03-12 16:13:56 -0700

    add small swapfile

--- systems/nix-shitfucker/hardware-configuration.nix
+++ systems/nix-shitfucker/hardware-configuration.nix
@@ -17,7 +17,12 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  swapDevices = [];
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 2048;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
