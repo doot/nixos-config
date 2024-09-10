b2021437d03ebfba0fd0cd34f570d9d538bc46d8
Jeremy Hauschildt <jeremy@jhauschildt.com>
2024-09-10 13:35:17 -0700

    fix: fix broken healthcheck

--- arion/freshrss/default.nix
+++ arion/freshrss/default.nix
@@ -23,7 +23,7 @@
         mem_limit = "1g";
         memswap_limit = "1g";
         healthcheck = {
-          test = ["CMD-SHELL" "curl --fail localhost:8666/ || exit 1"];
+          test = ["CMD-SHELL" "curl --fail localhost:80/ || exit 1"];
           interval = "30s";
           timeout = "15s";
           retries = 3;
