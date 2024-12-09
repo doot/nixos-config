e2651cf124e14b0627b6f26235c8a9cd40b684be
Jeremy Hauschildt <jeremy@jhauschildt.com>
2024-12-08 16:33:01 -0800

    feat: remove unstable module override for alloy now that system has been upgraded

--- systems/nix-media-docker/default.nix
+++ systems/nix-media-docker/default.nix
@@ -3,19 +3,12 @@
   lib,
   pkgs,
   modulesPath,
-  inputs,
   fqdn,
   ...
 }: {
   imports = [
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
     ./hardware-configuration.nix # Include the results of the hardware scan.
-    "${inputs.nixpkgs-unstable}/nixos/modules/services/monitoring/alloy.nix"
-  ];
-
-  # TODO: remove alloy from disabled modules and unstable override of the alloy service after next nixOS version is released. Currently the service is only available in unstable.
-  disabledModules = [
-    "services/monitoring/alloy.nix"
   ];
 
   environment.systemPackages = with pkgs; [
