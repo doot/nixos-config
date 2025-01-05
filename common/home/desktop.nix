4102d3c1ce272e3fdc40d703122e9d42213de5d6
Jeremy Hauschildt <jeremy@jhauschildt.com>
2025-01-04 22:55:03 -0800

    chore: fix hyprland config

--- common/home/desktop.nix
+++ common/home/desktop.nix
@@ -150,10 +150,12 @@
               size = 3
               passes = 1
           }
-          drop_shadow = yes
-          shadow_range = 4
-          shadow_render_power = 3
-          col.shadow = rgba(1a1a1aee)
+          shadow {
+            enabled = yes
+            range = 4
+            render_power = 3
+            color = rgba(1a1a1aee)
+          }
       }
 
       animations {
