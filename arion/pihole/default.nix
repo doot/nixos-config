8a76c3c35f3d0b816de8e680c0619ed3a53ccba5
Jeremy Hauschildt <jeremy@jhauschildt.com>
2025-02-22 17:29:03 -0800

    feat: update pihole env vars for v6 upgrade

--- arion/pihole/default.nix
+++ arion/pihole/default.nix
@@ -17,12 +17,12 @@
         ];
         environment = {
           TZ = "America/Los_Angeles";
-          DNSMASQ_LISTENING = "all";
+          FTLCONF_dns_listeningMode = "all";
           # PIHOLE_DNS_: "1.1.1.1;1.0.0.1;2606:4700:4700::1111;2606:4700:4700::1001"
-          PIHOLE_DNS_ = "9.9.9.9;149.112.112.112;2620:fe::fe;2620:fe::9";
+          FTLCONF_dns_upstreams = "9.9.9.9;149.112.112.112;2620:fe::fe;2620:fe::9";
           FTLCONF_LOCAL_IPV4 = "192.168.1.88";
           FTLCONF_LOCAL_IPV6 = "fe80::be24:11ff:fef7:aee4";
-          DNSSEC = "false"; # Temporarily disabled as it causes some issues with plex and other services
+          FTLCONF_dns_dnssec = "false"; # Temporarily disabled as it causes some issues with plex and other services
           # WEBPASSWORD: "set a secure password here or it will be random"
         };
         capabilities.NET_ADMIN = true;
