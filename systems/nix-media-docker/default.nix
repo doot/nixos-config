39b85463c8e251b09bf6affbebfbeb3a8aaa349e
Jeremy Hauschildt <jeremy@jhauschildt.com>
2026-06-01 17:43:22 -0700

    fix: correct reverse ngnix reverse proxy for librenms

--- systems/nix-media-docker/default.nix
+++ systems/nix-media-docker/default.nix
@@ -284,6 +284,10 @@ in {
         {
           name = "librenms";
           port = 7000;
+          extraConfig = ''
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto $scheme;
+          '';
         }
         {
           name = "maloja";
