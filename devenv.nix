ba260d2d886a1b4f9dc630eceded05247cc98747
Jeremy Hauschildt <jeremy@jhauschildt.com>
2026-08-24 22:29:29 -0700

    feat: extract common devenv configuration into the separate devenv-base repo

--- devenv.nix
+++ devenv.nix
@@ -1,35 +1,9 @@
-{pkgs, ...}: {
+_: {
+  # Base config is derived from the base profile of github:doot/devenv-base.
+  # See: devenv.yaml and https://github.com/doot/devenv-base.
+
   enterTest = ''
     echo "Running tests"
     nix flake check --override-input priv ./priv
   '';
-
-  languages = {
-    nix = {
-      enable = true;
-      lsp.enable = true;
-    };
-    shell = {
-      enable = true;
-      lsp.enable = true;
-    };
-  };
-
-  packages = [pkgs.github-cli];
-
-  git-hooks.hooks = {
-    commitizen.enable = true;
-
-    deadnix.enable = true;
-
-    actionlint.enable = true;
-
-    shellcheck.enable = true;
-
-    alejandra.enable = true;
-
-    statix.enable = true;
-
-    check-yaml.enable = true;
-  };
 }
